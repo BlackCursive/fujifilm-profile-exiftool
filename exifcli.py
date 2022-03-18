4ab103daeb94baaca257f057b881f2303998175f
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2022-03-18 19:17:46 -0400

    Updated Script and Args File

--- exifcli.py
+++ exifcli.py
@@ -1,8 +1,12 @@
 '''
 Change the Model Number in the EXIF Data - exiftool _DSF1234-xt4.RAF -model=X-T4
 '''
+import os
 import typer
 import subprocess
+
+# Current directory of where the script resides
+cwd = os.path.dirname(os.path.realpath(__file__))
 
 # Initializing typer.Typer app and removes default option from --help
 app = typer.Typer(add_completion=False)
@@ -10,35 +14,58 @@ app = typer.Typer(add_completion=False)
 # ExifTool Directory
 exiftool = "exiftool/exiftool"
 
-# Script will be applied to eith all JPG or RAF files
-jpg = "*.JPG *.jpg"
-raf = "*.RAF *.raf"
+# Script will be applied to either all JPG or RAF files
+jpg = "-ext jpg ."
+raf = "-ext raf ."
+
+# Acccept any case input from user
+jpgs = ["jpg", "JPG"]
+rafs = ["raf", "RAF"]
+
+#Argument file
+args = r"-@ args.txt"
 
 @app.command()
-def main(filetype: str = typer.Option(..., prompt="Are these JPG or RAF files?")):
-  if filetype == 'JPG':
-    cmd = (f"{exiftool} -Make '-Model' '-FilmMode' '-Saturation' {jpg}")
-  elif filetype == 'RAF':
-    cmd = (f"{exiftool} -Make '-Model' '-FilmMode' '-Saturation' {raf}")
+def main():
+  modeltype = typer.confirm("Change the camera model to an XT-4?")
+  if modeltype:
+    filetype = typer.prompt("Are you processing JPG or RAF files?")
+    if filetype in jpgs:
+      cmd = (f"{exiftool} {args} {jpg}")
+    elif filetype in rafs:
+      cmd = (f"{exiftool} {args} {raf}")
+      print(cmd)
+    else:
+      message = " is not one of the predefined extensions - jpg, JPG, raf or RAF\n."
+      ending = typer.style(f"\nn{filetype}", bold=True, underline=True, )
+      typer.secho(ending + message)
+      raise typer.Abort()
   else:
-    cmd = (f"{exiftool} -Make '-Model' '-FilmMode' '-Saturation' {raf} {jpg}")
+    raise typer.Abort()
 
-  # REMOVE ON UPLOAD ###
-  print(cmd)
 
-  p1 = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines = True)
+  process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=cwd)
 
-  out, err = p1.communicate()
+  # for line in process.stdout:
+  #   print(line.rstrip())
+  #   if "X-E1" in line:
+  #     print("WTF - YES YES YES")
 
-  if out:
-    print(f'\nOuput:\n {out}')
-  else:
-    print(f'error: {err}')
+  while True:
+    line = process.stdout.readline()
+    if "X-E1" in line:
+      print("WTF - YES YES YES")
+    if not line:
+        break
+    print(line.rstrip())
 
-  if p1.returncode == 0:
+  stdout, stderr  = process.communicate()
+
+  if not stdout:
+    typer.secho(f'\n{stderr}', fg=typer.colors.RED, bold=True)
+
+  if process.returncode == 0:
     typer.secho('Success', fg=typer.colors.GREEN, bold=True)
-  else:
-    typer.secho('Failed', fg=typer.colors.WHITE, bg=typer.colors.RED, bold=True)
 
 if __name__ == "__main__":
   app()
