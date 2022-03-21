33cd328d74d6726c9486aef96f2cc4bd2507c0ba
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2022-03-21 19:25:44 -0400

    Script Update / Args File Deletion

--- exifcli.py
+++ exifcli.py
@@ -2,8 +2,12 @@
 Change the Model Number in the EXIF Data - exiftool _DSF1234-xt4.RAF -model=X-T4
 '''
 import os
+import pandas as pd
 import typer
 import subprocess
+
+# sudo pip3 install tabulate
+from tabulate import tabulate
 
 # Current directory of where the script resides
 cwd = os.path.dirname(os.path.realpath(__file__))
@@ -23,49 +27,62 @@ jpgs = ["jpg", "JPG"]
 rafs = ["raf", "RAF"]
 
 #Argument file
-args = r"-@ args.txt"
+args = '-Model -FilmMode -Saturation'
 
 @app.command()
 def main():
-  modeltype = typer.confirm("Change the camera model to an XT-4?")
+  modeltype = typer.confirm("Change the camera model to an XT-4?", abort=True)
   if modeltype:
     filetype = typer.prompt("Are you processing JPG or RAF files?")
     if filetype in jpgs:
-      cmd = (f"{exiftool} {args} {jpg}")
+      cmd = (f"{exiftool} {args} {jpg} -csv")
     elif filetype in rafs:
-      cmd = (f"{exiftool} {args} {raf}")
-      print(cmd)
+      cmd = (f"{exiftool} {args} {raf} -csv")
     else:
       message = " is not one of the predefined extensions - jpg, JPG, raf or RAF\n."
       ending = typer.style(f"\nn{filetype}", bold=True, underline=True, )
       typer.secho(ending + message)
       raise typer.Abort()
-  else:
-    raise typer.Abort()
 
-
+  # Run Subprocess
   process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=cwd)
 
-  # for line in process.stdout:
-  #   print(line.rstrip())
-  #   if "X-E1" in line:
-  #     print("WTF - YES YES YES")
+  #Pass CSV output Exiftool into a Pandas dataframe
+  df = pd.read_csv(process.stdout)
 
-  while True:
-    line = process.stdout.readline()
-    if "X-E1" in line:
-      print("WTF - YES YES YES")
-    if not line:
-        break
-    print(line.rstrip())
+  # Sort dataframe and reset index
+  df.sort_values('SourceFile', inplace=True, ignore_index=True)
+
+  # Start row index from 1 instead of zero
+  df.index = df.index + 1
+
+  # Copy Film Simulations from Saturation tag to FilmMode tag / Change Output
+  def film_mode(df):
+      df['FilmMode'] = df['FilmMode'].replace(['F0/Standard (Provia)','F1b/Studio Portrait Smooth Skin Tone (Astia)', 'F2/Fujichrome (Velvia)' ],['Provia / Standard', 'Astia / Soft', 'Velvia / Vivid'])
+      df.loc[df['Saturation']=='None (B&W)', 'FilmMode'] = 'B&W'
+      df.loc[df['Saturation']=='B&W Red Filter', 'FilmMode'] = 'B&W Red Filter'
+      df.loc[df['Saturation']=='B&W Yellow Filter', 'FilmMode'] = 'B&W Yellow Filter'
+      df.loc[df['Saturation']=='B&W Green Filter', 'FilmMode'] = 'B&W Green Filter'
+      df.loc[df['Saturation']=='B&W Sepia', 'FilmMode'] = 'Sepia'
+      df.loc[df['Saturation']=='Acros', 'FilmMode'] = 'Acros'
+      df.loc[df['Saturation']=='Acros Red Filter', 'FilmMode'] = 'Acros Red Filter'
+      df.loc[df['Saturation']=='Acros Yellow Filter', 'FilmMode'] = 'Acros Yellow Filter'
+      df.loc[df['Saturation']=='Acros Green Filter', 'FilmMode'] = 'Acros Green Filter'
+      df.rename(columns = {'FilmMode':'Film Mode'}, inplace = True)
+      df.fillna('N/A', inplace=True)
+      df.drop('Saturation', axis=1, inplace=True)
+  film_mode(df)
+
+  # Display formated Pandas dataframe in the terminal
+  print(tabulate(df, headers=df.columns, tablefmt="fancy_grid",  stralign=("center")))
 
   stdout, stderr  = process.communicate()
 
   if not stdout:
-    typer.secho(f'\n{stderr}', fg=typer.colors.RED, bold=True)
+    typer.secho(f'\n{stderr}', fg=typer.colors.BLACK, bold=True)
 
-  if process.returncode == 0:
-    typer.secho('Success', fg=typer.colors.GREEN, bold=True)
+  # if process.returncode == 0:
+  #   typer.secho('Success', fg=typer.colors.GREEN, bold=True)
 
 if __name__ == "__main__":
   app()
