21d8bb04d86956f441171a774ff0a50ae905cf04
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2022-04-05 21:27:33 -0400

    Update exifcli.py

--- exifcli.py
+++ exifcli.py
@@ -1,26 +1,38 @@
 '''
-Change the Model Number in the EXIF Data - exiftool _DSF1234-xt4.RAF -model=X-T4
+Change the Model Number in the EXIF Data to an X-T4
 '''
 import os
 import pandas as pd
+import re
 import time
 import typer
 import subprocess
 
-# sudo pip3 install tabulate
+from pathlib import Path
 from tabulate import tabulate
-
-# Current directory of where the script resides
-cwd = os.path.dirname(os.path.realpath(__file__))
 
 # Initializing typer.Typer app and removes default option from --help
 app = typer.Typer(add_completion=False)
 
+# Current directory of where the script resides
+home_dir = Path.cwd()
+
+# RAW Files Directory
+raf_files = home_dir / 'raf_files'
+
+# If raf_files folder does not exist then it is created
+if Path(raf_files).is_dir():
+  pass
+else:
+  print(f"The following folder has been created: {raf_files}")
+  raf_files.mkdir(parents=False, exist_ok=True)
+  time.sleep( 1 )
+
 # ExifTool Directory
-exiftool = "exiftool/exiftool"
+exiftool = home_dir / "exiftool/exiftool"
 
 # Script will be applied to either all JPG or RAF files
-raf = "-ext raf ."
+raf = "-ext raf"
 
 # Acccept any case input from user
 rafs = ["raf", "RAF"]
@@ -30,10 +42,29 @@ args = '-Model -FilmMode -Saturation'
 
 @app.command()
 def main():
+  jpg_file_ext = [".jpg", ".jpeg", ".JPG"]
+  raf_file_ext = [".raf", ".RAF"]
+
+  # List comprehension that appends the file extensions of every file in the raf_files directory
+  raf_dir_ext = [raf_dir_file_ext.suffix for raf_dir_file_ext in Path(raf_files).iterdir()]
+
+  # - list items within another list
+  jpg_dir_ext_valid = any(jpg_file in jpg_file_ext for jpg_file in raf_dir_ext)
+  raf_dir_ext_valid = any(raw_file in raf_file_ext for raw_file in raf_dir_ext)
+
+  # Checks if jpgs are in the raf_files directory
+  if jpg_dir_ext_valid:
+    typer.secho(f"Please note there are files with either one of these extensions{jpg_file_ext}\nin the raf_files folder but they will not be affected.\n", fg=typer.colors.RED, bold=True)
+
+  # Checks if rafs are in the raf_files directory
+  if not raf_dir_ext_valid:
+    typer.secho(f"There aren't any Fujifilm RAW files in the 'raf_files' folder.\n", fg=typer.colors.RED, underline=True, bold=True)
+    raise typer.Abort()
+
   cmd_one = (f"{exiftool} {args} {raf} -csv")
-  filetype = typer.prompt("If you would like to change the camera model of your Fujifilm RAW files to an XT-4?\nType in the extenstion 'RAF'")
+  filetype = typer.prompt("If you would like to change the camera model of your Fujifilm RAW files to an XT-4 place\nthem in the raf_files folder then please type in the extenstion 'RAF' for confirmation")
   if filetype in rafs:
-    cmd_one = (f"{exiftool} {args} {raf} -csv")
+    cmd_one = (f"{exiftool} {args} {raf_files} {raf} -csv")
   else:
     message = " is not one of the predefined extensions - raf or RAF.\n"
     ending = typer.style(f"\nn{filetype}", bold=True, underline=True, )
@@ -41,7 +72,7 @@ def main():
     raise typer.Abort()
 
   # Run 1st Subprocess
-  pre_process = subprocess.Popen(cmd_one, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=cwd)
+  pre_process = subprocess.Popen(cmd_one, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=home_dir)
 
   #Pass CSV output Exiftool into a Pandas dataframe
   df_pre_process = pd.read_csv(pre_process.stdout)
@@ -49,17 +80,17 @@ def main():
   # Rename column
   df_pre_process.rename(columns = {'Model':'Model (Before)'}, inplace = True)
 
-  print(tabulate(df_pre_process, headers=df_pre_process.columns, tablefmt="fancy_grid",  stralign=("center")))
+  ### print(tabulate(df_pre_process, headers=df_pre_process.columns, tablefmt="fancy_grid",  stralign=("center")))
 
   # Run 2nd Subprocess - convert model
   cmd_two = (f"{exiftool} -model=X-T4 {raf} -csv")
-  mid_process = subprocess.Popen(cmd_two, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=cwd)
+  mid_process = subprocess.Popen(cmd_two, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=home_dir)
 
   #Sleep for 2 seconds
   time.sleep( 2 )
 
   # Run 1st Subprocess again
-  post_process = subprocess.Popen(cmd_one, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=cwd)
+  post_process = subprocess.Popen(cmd_one, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=home_dir)
 
   #Pass CSV output Exiftool into a Pandas dataframe
   df_post_process = pd.read_csv(post_process.stdout)
@@ -83,14 +114,23 @@ def main():
     df_any_process.drop('Saturation', axis=1, inplace=True)
     df_any_process.fillna('N/A', inplace=True)
     df_any_process = df_any_process[['SourceFile', 'Film Mode', 'Model (Before)', 'Model']]
+    # print(Path(raf_files).parent)
+    # print(Path(raf_files).name)
     print(tabulate(df_any_process, headers=df_any_process.columns, tablefmt="fancy_grid", stralign=("center")))
   post_film_mode(df_post_process)
 
   # Subprocess PIPE outputs
   stdout_mid_process, stderr_mid_process  = mid_process.communicate()
 
+  # Standard ouput & suffix on process files
   if not stdout_mid_process:
     typer.secho(f'\n{stderr_mid_process}', fg=typer.colors.BLACK, bold=True)
+    typer.secho(f"Backups are made with the suffix '_original' added to the filename. \n", fg=typer.colors.BLACK, bold=True)
+
+  # Close standard output
+  pre_process.stdout.close()
+  mid_process.stdout.close()
+  post_process.stdout.close()
 
 if __name__ == "__main__":
   app()
